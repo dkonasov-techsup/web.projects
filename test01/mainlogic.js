3095ecf9f2a3d073e22ffa01baa646812899cdb4
dkonasov-techsup <dkonasov@geoscan.aero>
2022-10-26 21:06:06 +0300

    Update mainlogic.js

--- test01/mainlogic.js
+++ test01/mainlogic.js
@@ -1,6 +1,19 @@
 "use strict"
-let login = prompt("Enter login","User");
+// let login = prompt("Enter login","User");
 
-// mesage1 = 'You are using a management system';
+// // mesage1 = 'You are using a management system';
 
-alert(`Login ${login} accept`);
+// alert(`Login ${login} accept`);
+
+const obj1 = {
+	val1:10,
+	val2:20,
+	val3:30
+}
+alert(obj1.val4);
+
+const obj2 = obj1;
+obj2.val4 = 40;
+
+
+alert(obj1.val4);
