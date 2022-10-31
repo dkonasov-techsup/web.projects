7a152eb60396523e2961ec2e486b088ed99a957d
dkonasov-techsup <d.konasov@geoscan.aero>
2022-10-31 16:03:45 +0300

    сhange btn color

--- test01/mainlogic.js
+++ test01/mainlogic.js
@@ -6,7 +6,10 @@
 // const btn3 = document.getElementById('btnData');
 
 const b1Buttons = document.querySelectorAll('.b1_button');
+const b1CheckBox = document.querySelectorAll('.b1_switch');
 console.log(b1Buttons);
+console.log(b1CheckBox);
+
 
 function btnClick(text){
 	console.log(text);
@@ -15,11 +18,11 @@ function btnClick(text){
 function btnMouseEnter(event){
 	// console.log('mouseenter');
 	// console.log(event.target.id);
-	this.style.color="#0ab0ee";
-	this.style.borderColor="#0ab0ee";
-	this.style.boxShadow = "inset 0px 0px 8px 0px #0ab0ee";
+	this.style.color="#33d0ff";
+	this.style.borderColor="#33d0ff";
+	this.style.boxShadow = "inset 0px -2px 20px 10px #161616";
 	// console.log(event.target.children[0]);
-	event.target.children[0].style.stroke="#0ab0ee";	
+	event.target.children[0].style.stroke="#33d0ff";	
 }
 
 function btnMouseLeave(){
@@ -31,21 +34,10 @@ function btnMouseLeave(){
 	event.target.children[0].style.stroke="";	
 }
 
-// Присваивание EventListener в цикле
+// Присваивание EventListener нопкам в цикле
 b1Buttons.forEach(b1_button => {
 	 b1_button.addEventListener('mouseenter', btnMouseEnter);
 	 b1_button.addEventListener('mouseleave', btnMouseLeave)
 })
-
-
-
-// function btnMouseOver(text){
-// 	// alert(text);
-// 	btn1.style.color="blue";
-// }
-
-// function btnMouseLeave(){	
-// 	btn1.style.color='';
-// }
 
 
