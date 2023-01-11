c9cbababc4b4ac48aa5c9e95a55baf46d0df8a4c
dkonasov-techsup <d.konasov@geoscan.aero>
2023-01-11 17:49:49 +0300

    some changes

--- controlPanel/pointer.js
+++ controlPanel/pointer.js
@@ -3,13 +3,36 @@
 const workSpace = document.querySelector('.pointer_workspace')
 
 workSpace.addEventListener('mousemove', pointerActive)
+workSpace.addEventListener('mouseenter', pointerActive)
+workSpace.addEventListener('mouseleave', pointerActive)
+
+const rulerX = document.querySelector('.rulerX');
+const rulerY = document.querySelector('.rulerY');
+const positionCell = document.querySelector('.posValue');
 
 function pointerActive(event){
-	// getPosition(event)
-	// drawRulers()
-	rulersTracking(event)
+
+	if(event.type == "mouseenter"){
+		rulersShow();
+	}
+	if(event.type == "mouseleave"){
+		rulersHide();
+	}
+	rulersTracking(event);	
 }
 
+
+function rulersShow(){	
+	rulerX.style.display = "block";
+	rulerY.style.display = "block";
+}
+
+function rulersHide(){	
+	rulerX.style.display = "none";
+	rulerY.style.display = "none";
+	// positionCell.textContent = "0 dfsdf";
+	console.log(positionCell.textContent)
+}
 
 function getPosition(event){
 	const target = event.target.getBoundingClientRect();	
@@ -19,13 +42,10 @@ function getPosition(event){
 }
 
 function rulersTracking(event){
-	let coords = getPosition(event)
-	console.log(coords[0],coords[1]);
+	let coords = getPosition(event)		
 
-	const rulerX = document.querySelector('.rulerX');
-	const rulerY = document.querySelector('.rulerY');	
+	rulerX.style.left = coords[0] + "px";
+	rulerY.style.top = coords[1] + "px";		
 
-	// rulerX.style.display = "block";
-	rulerX.style.marginLeft = coords[0] + "px";
-	// rulerY.style.marginTop = coords[1] + "px";
+	positionCell.textContent = "x: "+coords[0]+" y: "+coords[1];
 }
