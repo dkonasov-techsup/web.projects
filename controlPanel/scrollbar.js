4404405aa5b7b88323f19c10be0599a0cf34db7f
dkonasov-techsup <d.konasov@geoscan.aero>
2023-01-16 18:00:52 +0300

    scrollbar.js

--- controlPanel/scrollbar.js
+++ controlPanel/scrollbar.js
@@ -30,25 +30,42 @@ const thumbs = document.querySelectorAll('.scrollbar_thumb');
 
 for(let thumb of thumbs){	
 	thumb.addEventListener('mousedown', startScroll);
-	thumb.addEventListener('mouseup', stopScroll);
+	// thumb.addEventListener('mouseup', stopScroll);
 	thumb.ondragstart = function(){
 		return false;
 	}	
 }
 
 function startScroll(event){
-	console.log('fff');
+	console.log('startScroll');
+
+	let activeThumb = event.target;
+	let activeTrack = event.target.closest('.scrollbar_track')
+	
+
 	// let pointer = event.target.closest('#pointer');
 
-	document.addEventListener('mousemove', startScroll)
+	document.addEventListener('mousemove', moveThumb);
+	document.addEventListener('mouseup', stopThumb);
+
+	function moveThumb(event){
+
+		let track = activeTrack.getBoundingClientRect()
+
+		activeThumb.style.left = ((event.clientX - track.left) + activeThumb.offsetWidth / 2) + "px";
+		 
+
+	}
+
+	function stopThumb(event){
+		console.log('stopScroll');
+		document.removeEventListener('mousemove', moveThumb);
+		document.removeEventListener('mouseup', stopThumb);
+		console.log(activeThumb);
+	}
+
 }
 
 
-function startScroll(event){
-	console.log(event.target);
-}
 
-function stopScroll(event){
-	console.log('stop');
-	document.removeEventListener('mousemove', startScroll);	
-}
+
