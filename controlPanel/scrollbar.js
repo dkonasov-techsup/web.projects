8674f12857e865e88c9b3e972ef50fe4e95ecdde
dkonasov-techsup <d.konasov@geoscan.aero>
2023-01-17 18:15:17 +0300

    scrollbar.js

--- controlPanel/scrollbar.js
+++ controlPanel/scrollbar.js
@@ -1,82 +1,87 @@
-"use strict"
+(function(){
+	"use strict"
 
-let scrList = document.querySelectorAll('.scrolable');
+	class ScrollBar{
+		constructor(container){			
+		}
 
-
-// Заполняем массив с родителями имеющими внутри прокручиваемый дочерний элемент 
-
-const contList = [];
-function getContainer(scrList){	
-	scrList.forEach((item,index)=>{
-		contList[index] = item.parentNode;		
-	})
-}
-getContainer(scrList)
-
-
-// Добавляем контейнер с скролбаром
-function addScrollbar(contList){
-	const scrollbar = '<div class="scrollbar"><div class="scrollbar_track"><div class="scrollbar_thumb"></div></div></div>'
-	for(const cont of contList){
-		console.log(cont);
-		cont.insertAdjacentHTML('beforeend', scrollbar);
 	}
-}
-addScrollbar(contList)
 
-
-// Отслеживаем нажатие на scrollbar_thumb
-const thumbs = document.querySelectorAll('.scrollbar_thumb');
-
-for(let thumb of thumbs){	
-	thumb.addEventListener('mousedown', startScroll);
-	// thumb.addEventListener('mouseup', stopScroll);
-	thumb.ondragstart = function(){
-		return false;
-	}	
-}
-
-function startScroll(event){
-	console.log('startScroll');
-
-	let activeThumb = event.target;
-	let activeTrack = event.target.closest('.scrollbar_track')
 	
+	
+	// Заполняем массив с блоками имеющими внутри прокручиваемый дочерний элемент
 
-	// let pointer = event.target.closest('#pointer');
+	// const scrList = document.querySelectorAll('.scrolable');
+	// const contList = [];
 
-	document.addEventListener('mousemove', moveThumb);
-	document.addEventListener('mouseup', stopThumb);
-
-	function moveThumb(event){
-
-		let thumbPos = Number(activeThumb.style.left);
-		console.log(thumbPos);
-
-		// if (thumbPos <= 20){
-		// 	return false;
-		// 	// stopThumb();
-		// }
-
-		console.log(activeThumb.style.left);
+	// function getContainer(scrList){	
+	// 	scrList.forEach((item,index)=>{
+	// 		contList[index] = item.parentNode;		
+	// 	})
+	// }
+	// getContainer(scrList)
 
 
-		let track = activeTrack.getBoundingClientRect()
+	// // Добавляем контейнер с скролбаром
+	// function addScrollbar(contList){
+	// 	const scrollbar = '<div class="scrollbar"><div class="scrollbar_track"><div class="scrollbar_thumb"></div></div></div>'
+	// 	for(const cont of contList){
+	// 		console.log(cont);
+	// 		cont.insertAdjacentHTML('beforeend', scrollbar);
+	// 	}
+	// }
+	// addScrollbar(contList)
 
-		activeThumb.style.left = ((event.clientX - track.left) + activeThumb.offsetWidth / 2) + "px";
-		 
 
-	}
+	// // Отслеживаем нажатие на scrollbar_thumb
+	// const thumbs = document.querySelectorAll('.scrollbar_thumb');
 
-	function stopThumb(event){
-		console.log('stopScroll');
-		document.removeEventListener('mousemove', moveThumb);
-		document.removeEventListener('mouseup', stopThumb);
+	// for(let thumb of thumbs){	
+	// 	thumb.addEventListener('mousedown', startScroll);
+	// 	// thumb.addEventListener('mouseup', stopScroll);
+	// 	thumb.ondragstart = function(){
+	// 		return false;
+	// 	}	
+	// }
+
+	// function startScroll(event){
+	// 	console.log('startScroll');
+
+	// 	let activeThumb = event.target;
+	// 	let activeTrack = event.target.closest('.scrollbar_track')
 		
-	}
+		
+	// 	document.addEventListener('mousemove', moveThumb);
+	// 	document.addEventListener('mouseup', stopThumb);
 
-}
+	// 	function moveThumb(event){
 
+	// 		let trackRect = activeTrack.getBoundingClientRect()
+	// 		let thumbRect = activeThumb.getBoundingClientRect()
+	// 		let thumbPos = window.getComputedStyle(activeThumb).left;
 
+	// 		console.log(trackRect.left);
+	// 		console.log(thumbRect.left);
+			
+	// 		if(thumbRect.left < trackRect.left){
+	// 			console.log('<')
+
+	// 		}
+
+	// 		activeThumb.style.left = ((event.clientX - trackRect.left) - activeThumb.offsetWidth / 2) + "px";
+			 
+
+	// 	}
+
+	// 	function stopThumb(event){
+	// 		console.log('stopScroll');
+	// 		document.removeEventListener('mousemove', moveThumb);
+	// 		document.removeEventListener('mouseup', stopThumb);
+			
+	// 	}
+
+	// }
+
+}())
 
 
